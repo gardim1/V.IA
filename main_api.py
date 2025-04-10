d07c43b6295d717409301057810157836094f079
gardim1 <vini.gardim1@gmail.com>
2025-04-10 19:21:47 -0300

    `fix` arrumando erro de CORS

--- main_api.py
+++ main_api.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
 from vector_utils import get_retriever
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 model = Ollama(model="llama3.2")
 
