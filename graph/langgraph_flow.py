bc2cb1cef58d5872b855829ca236d60a2c37a974
gardim1 <vini.gardim1@gmail.com>
2025-06-25 14:02:52 -0300

    `fix`: Ajusta nodes

--- graph/langgraph_flow.py
+++ graph/langgraph_flow.py
@@ -1,11 +1,11 @@
+from typing import TypedDict
 from langgraph.graph import StateGraph, END
 from langgraph.prebuilt import ToolNode
-from graph.roteador import roteador_tool
 
+from graph.roteador import roteador_tool
 from agents.cte_mdfe_agent import cte_mdfe_agent
 from agents.roteirizacao_agent import roteirizacao_agent
-from agents.geral_agent import geral_agent  
-from typing import TypedDict
+from agents.geral_agent import geral_agent
 
 class GraphState(TypedDict):
     pergunta: str
@@ -14,18 +14,15 @@ class GraphState(TypedDict):
 
 builder = StateGraph(GraphState)
 
-
-builder.add_node("roteador", ToolNode(roteador_tool))
-builder.add_node("cte_mdfe",      cte_mdfe_agent)
-builder.add_node("roteirizacao",  roteirizacao_agent)
-builder.add_node("geral",         geral_agent)
+builder.add_node("roteador", ToolNode([roteador_tool]))
+builder.add_node("cte_mdfe", cte_mdfe_agent)
+builder.add_node("roteirizacao", roteirizacao_agent)
+builder.add_node("geral", geral_agent)
 
 builder.set_entry_point("roteador")
-
-builder.add_edge("roteador", lambda s: s["next"])
-
-builder.add_edge("cte_mdfe",     END)
+builder.add_edge("roteador", lambda state: state["next"])
+builder.add_edge("cte_mdfe", END)
 builder.add_edge("roteirizacao", END)
-builder.add_edge("geral",        END)
+builder.add_edge("geral", END)
 
 langgraph_flow = builder.compile()
