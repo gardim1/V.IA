6b9fdeae6905ee98c8dbb31308c6ceb2d7296396
gardim1 <vini.gardim1@gmail.com>
2025-06-25 14:11:19 -0300

    `fix`: Altera nodes pt3

--- graph/langgraph_flow.py
+++ graph/langgraph_flow.py
@@ -2,9 +2,9 @@ from typing import TypedDict
 from langgraph.graph import StateGraph, END
 
 from graph.roteador import roteador_tool
-from agents.cte_mdfe_agent import cte_mdfe_agent
-from agents.roteirizacao_agent import roteirizacao_agent
-from agents.geral_agent import geral_agent
+from agents.cte_mdfe_agent      import cte_mdfe_agent
+from agents.roteirizacao_agent  import roteirizacao_agent
+from agents.geral_agent         import geral_agent
 
 class GraphState(TypedDict):
     pergunta: str
@@ -13,13 +13,26 @@ class GraphState(TypedDict):
 
 builder = StateGraph(GraphState)
 
-builder.add_node("roteador",     roteador_tool)  
+# nós
+builder.add_node("roteador",     roteador_tool)
 builder.add_node("cte_mdfe",     cte_mdfe_agent)
 builder.add_node("roteirizacao", roteirizacao_agent)
 builder.add_node("geral",        geral_agent)
 
 builder.set_entry_point("roteador")
-builder.add_edge("roteador", lambda s: s["next"])
+
+# roteamento condicional
+builder.add_conditional_edges(
+    "roteador",
+    lambda s: s["next"],
+    {
+        "cte_mdfe":     "cte_mdfe",
+        "roteirizacao": "roteirizacao",
+        "geral":        "geral",
+    },
+)
+
+# nós finais
 builder.add_edge("cte_mdfe",     END)
 builder.add_edge("roteirizacao", END)
 builder.add_edge("geral",        END)
